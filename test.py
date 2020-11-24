89257e041c62c4ada3cd68d5634e5a9e494ca1b1
djhoo <djhoo@sina.com>
2020-11-24 20:02:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,31 +15,50 @@ df2['rate'] = 0
 df2['rate'] = (df2['close'].shift(0) - df2['close'].shift(1))/df2['close'].shift(1)
 #df3= df2[df2['rate']>0.096]
 
-#当日的最大值率，有最大值除以开始的值
+#当日的最大值率，用最大值除以开始的值
 df2['highrate'] = (df2['high']-df2['open'])/df2['open']
 df2['lowrate'] = (df2['low']-df2['open'])/df2['open']
 
 
 #get the object row
-mask = (df2['rate'].shift(0)>0.096)|(df2['rate'].shift(1)>0.096)|(df2['rate'].shift(2)>0.096)|(df2['rate'].shift(3)>0.096)|(df2['rate'].shift(4)>0.096)
+#mask = (df2['rate'].shift(0)>0.096)|\
+#        ((df2['rate'].shift(1)>0.096)&(df2['rate'].shift(0)<=0.096))|\
+#            ((df2['rate'].shift(2)>0.096)&(df2['rate'].shift(0)<=0.096))|\
+#                ((df2['rate'].shift(3)>0.096)&(df2['rate'].shift(0)<=0.096))|\
+#                    ((df2['rate'].shift(4)>0.096)&(df2['rate'].shift(0)<=0.096))
+#mask = (df2['rate'].shift(0)<-0.096)|(df2['rate'].shift(1)<-0.096)|(df2['rate'].shift(2)<-0.096)|(df2['rate'].shift(3)<-0.096)|(df2['rate'].shift(4)<-0.096)
+#mask = ((df2['rate'].shift(0)>0.096)|(df2['rate'].shift(1)>0.096)|(df2['rate'].shift(2)>0.096)|(df2['rate'].shift(3)>0.096)|(df2['rate'].shift(4)>0.096))&(df2['rate'].shift(-1)<=0.096)
+mask = ((df2['rate'].shift(0)>0.096)&(df2['rate'].shift(-1)<=0.096)&(df2['rate'].shift(-2)<=0.096)&(df2['rate'].shift(-3)<=0.096)&(df2['rate'].shift(-4)<=0.096))|\
+    ((df2['rate'].shift(0)<=0.096)&(df2['rate'].shift(1)>0.096)&(df2['rate'].shift(-1)<=0.096)&(df2['rate'].shift(-2)<=0.096)&(df2['rate'].shift(-3)<=0.096))|\
+        ((df2['rate'].shift(0)<=0.096)&(df2['rate'].shift(1)<=0.096)&(df2['rate'].shift(2)>0.096)&(df2['rate'].shift(-1)<=0.096)&(df2['rate'].shift(-2)<=0.096))|\
+            ((df2['rate'].shift(0)<=0.096)&(df2['rate'].shift(1)<=0.096)&(df2['rate'].shift(2)<=0.096)&(df2['rate'].shift(3)>0.096)&(df2['rate'].shift(-1)<=0.096))|\
+                ((df2['rate'].shift(0)<=0.096)&(df2['rate'].shift(1)<=0.096)&(df2['rate'].shift(2)<=0.096)&(df2['rate'].shift(3)<=0.096)&(df2['rate'].shift(4)>0.096))
+
 df3 = df2[mask]
-print(df3)
 
-print(df3.to_numpy())
+df4 = df3[['date','volume','highrate','lowrate','rate']]
+print(df4)
 
-df3.to_csv('foo1.csv')
+rowsize = df4.shape[0]
+leftsize = rowsize % 5
+df4.drop(df4.tail(leftsize).index)
+
+print(df4)
+df4.to_csv('foo1.csv')
+
+np4 = df4.to_numpy();
+np5 = np4.reshape((25,));
+print(np5)
+
+
+print(rowsize)
+print(leftsize)
+
+
 
 #df2[mask].to_csv('foo1.csv')
 np3 = df3.to_numpy();
-np4 = np3.reshape((50,));
-
-print(np4)
+#np4 = np3.reshape((50,));
 
 print ("hello world")
-df4 = df[['open','close']]
-df5 = df.loc[:,'open':'close']
 #print(df3)
-print(df4)
-print(df)
-
-
