b073ff8cb6cf9ec3efa69ad8fad1e0a4674af904
djhoo <djhoo@sina.com>
2020-11-25 19:21:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import os
 import sys
 import pandas as pd
 import gugu as gg
+import numpy as np
 
 obj = gg.StockData('002231') 
-obj.history(start='2017-10-02', end='2020-11-17')
+obj.history(start='2017-01-01', end='2019-12-30')
 df = obj._data
 print(df)
 #print(df[(df.close-df.open)/df.open > 0.096])
@@ -44,21 +45,29 @@ leftsize = rowsize % 5
 df4.drop(df4.tail(leftsize).index)
 
 print(df4)
-df4.to_csv('foo1.csv')
+#df4.to_csv('foo1.csv')
 
 np4 = df4.to_numpy();
-np5 = np4.reshape((25,));
-print(np5)
+np5 = np4.reshape((-1,25));
+#df5 = df4.values.reshape((25,-1))
+#numpy.savetxt("foo.csv", a, delimiter=",")
+#pd.DataFrame(np5).to_csv("foo1.csv")
+pd5 = pd.DataFrame(np5)
+print(pd5)
+pd6 = pd5.drop(pd5.columns[[5,10,15,20]], axis=1)
+pd6.columns = ['date','volume1','highrate1','lowrate1','rate1','volume2','highrate2','lowrate2','rate2','volume3','highrate3','lowrate3',\
+    'rate3','volume4','highrate4','lowrate4','rate4','volume5','highrate5','lowrate5','rate5']
+
+pd6['volume1'] = pd6['volume1'] / pd6['volume2']
+pd6['volume3'] = pd6['volume3'] / pd6['volume2']
+pd6['volume4'] = pd6['volume4'] / pd6['volume2']
+pd6['volume5'] = pd6['volume5'] / pd6['volume2']
+pd6['volume2'] = 1
+
+print(pd6)
 
 
-print(rowsize)
-print(leftsize)
-
-
-
-#df2[mask].to_csv('foo1.csv')
-np3 = df3.to_numpy();
-#np4 = np3.reshape((50,));
+pd6.to_csv('foo1.csv')
 
 print ("hello world")
 #print(df3)
